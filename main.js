783c5455ee52ff2e2d45bf4723069826686aa68e
DemaHamson <linliangjie1126@163.com>
2021-11-19 14:33:29 +0800

    upload
    upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+"auto";
+
+
+function _tap(item)
+
+{
+
+  click(item.bounds().centerX(),item.bounds().centerY());
+
+}
+
+function start(kw) {
+  if(textContains(kw).exists()) {
+    toast(" go go");
+    var a= textContains(kw).findOne();
+    _tap(a);
+    text("立即购买").waitFor();
+    click(100,2200)
+    sleep(3000)
+    if (textContains("提交订单").exists()) {
+        var c=textContains("提交订单").findOne();
+        _tap(c)
+    }
+  }
+  
+}
+
+
+var task=setInterval(function(){
+    toast("ready")
+    device.wakeUp()
+    var date=new Date()
+    var hour=date.getHours()
+    var day=date.getDay()
+    
+    if(day==5 && hour>=14) {
+        clearInterval(task)
+        start("乐乐茶15元")
+    }
+
+},1000)
+
+
+
